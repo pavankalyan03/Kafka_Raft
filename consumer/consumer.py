9c650616f4949d8b60da018eda22939b380fcd4e
Pavan Kalyan <68956154+pavankalyan03@users.noreply.github.com>
2025-03-18 02:04:17 +0530

    Update consumer.py

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,11 +3,11 @@ from pymongo import MongoClient
 import json
 import logging
 import time
-# Configure logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Kafka Consumer Configuration (Docker-aware)
+# Kafka Consumer Configuration which directly runs inside docker
 consumer = KafkaConsumer(
     'ecommerce-events',
     bootstrap_servers='kafka:9092',  # Changed to Docker service name
@@ -19,14 +19,14 @@ consumer = KafkaConsumer(
     heartbeat_interval_ms=10000
 )
 
-# MongoDB Connection with error handling
+# MongoDB Connection and i also tried error handling
 try:
     client = MongoClient(
         'mongodb://mongodb:27017',
         serverSelectionTimeoutMS=5000,
         connectTimeoutMS=3000
     )
-    client.admin.command('ismaster')  # Test connection
+    client.admin.command('ismaster')  # here we testing the connection
     db = client['ecommerce']
     collection = db['events']
     logger.info("Connected to MongoDB successfully")
@@ -39,7 +39,6 @@ logger.info("Consumer ready. Waiting for messages...")
 try:
     for message in consumer:
         try:
-            # Add data validation here
             if not all(key in message.value for key in ['event', 'order_id', 'amount']):
                 raise ValueError("Invalid message format")
                 
@@ -50,7 +49,7 @@ try:
             })
             logger.info(f"Inserted order {message.value['order_id']}")
             
-            # Commit offset only after successful write
+            #commiting
             consumer.commit()
         except Exception as e:
             logger.error(f"Error processing message: {e}")
